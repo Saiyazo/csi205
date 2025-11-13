6906419045484babe06b6dfe2605839445dd9bc5
Saiyazo <champnachapat@gmail.com>
2025-11-13 23:02:07 +0700

    delete source code

--- src/layouts/AppHeader.jsx
+++
@@ -1,9 +0,0 @@
-const AppHeader = () => {
-    return (
-        <div className="d-flex justify-content-center align-items-center text-center bg-primary text-white py-4">
-            <h1>CSI205-L การพัฒนาโปรแกรมส่วนหน้า</h1>
-        </div >
-    );
-}
-
-export default AppHeader;
